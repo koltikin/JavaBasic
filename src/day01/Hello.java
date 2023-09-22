74eb13def72f316cda35911be34aaeb0fe393218
ziya <koltikin210@gmail.com>
2023-09-22 11:41:22 +0300

    some hello world review with friends

--- src/day01/Hello.java
+++ src/day01/Hello.java
@@ -6,14 +6,18 @@ public class Hello {
 //        System.out.println("hello world");
 
         String x = "how are you";
+        int age = 32;
 
-        System.out.printf("hello \"ziya\" %s\n",x);
-        System.out.printf("hello \'alim\' %s\n",x);
-        System.out.printf("hello\t\t\t\tyusuf %s",x);
+//        System.out.printf("hello \"ziya\" %s\n",x);
+//        System.out.printf("hello \'alim\' %s\n",x);
+//        System.out.printf("hello\t\t\t\tyusuf %s",x);
 //        System.out.printf("hello ziya are you %d years old",32);
 
 //        System.out.println("hello ziya \nhow are you");
 
+        System.out.println("hello ziya " + x);
+
+        System.out.println("hello ziya are you " + age + " years old");
 
 
 
